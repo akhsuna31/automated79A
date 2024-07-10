679e131255dc392f0c374fcc5a93420e2ee2a5bc
Arvind Kumar Meena <142485855+Aarv36@users.noreply.github.com>
2024-07-10 17:13:01 +0530

    Update admin.py

--- exam_form/admin.py
+++ exam_form/admin.py
@@ -3,9 +3,11 @@ from django.utils.html import format_html
 from .models import ExaminerApplication, Officer
 
 class ExaminerApplicationAdmin(admin.ModelAdmin):
+
     change_list_template = "admin/examinerapplication_changelist.html"
     list_display = ('application_number', 'user', 'department_name', 'is_verified', 'is_submitted')  # Adjust as needed for individual listing
     search_fields = ('user__username', 'department_name', 'application_number')
+    list_filter = ('is_submitted', 'is_verified')
     actions = ['mark_as_verified', 'allow_edit']
 
     def mark_as_verified(self, request, queryset):
@@ -19,9 +21,11 @@ class ExaminerApplicationAdmin(admin.ModelAdmin):
     mark_as_verified.short_description = "Mark selected applications as verified"
     allow_edit.short_description = "Allow selected applications to be edited by users"
 
-    def changelist_view(self, request, extra_context=None):
-        extra_context = extra_context or {}
 
+    def changelist_view(self, request, extra_context=None):
+
+        extra_context = extra_context or {}
+        officers = Officer.objects.all()
         total_filled_verified = ExaminerApplication.objects.filter(is_verified=True, is_submitted=True).count() #is_submitted=True
         partial_filled = ExaminerApplication.objects.filter(is_verified=False, is_submitted=False).count()
         filled_not_verified = ExaminerApplication.objects.filter(is_verified=False, is_submitted=True).exclude(detailed_description='').count()
@@ -29,8 +33,13 @@ class ExaminerApplicationAdmin(admin.ModelAdmin):
         extra_context['verified_count'] = total_filled_verified
         extra_context['partial_filled_count'] = partial_filled
         extra_context['unverified_count'] = filled_not_verified
+        extra_context['officers'] = officers
 
         return super().changelist_view(request, extra_context=extra_context)
 
+
+class OfficerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rank')
+
 admin.site.register(ExaminerApplication, ExaminerApplicationAdmin)
-admin.site.register(Officer)
+admin.site.register(Officer, OfficerAdmin)
